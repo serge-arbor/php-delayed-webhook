be722d3af2cd2af825652e9fddf7d064c5b92829
arbora <fuckall151055>
2021-10-25 03:24:15 +0300

    Implement delayed message handler

--- src/Timer/Message/TimerMessageHandler.php
+++ src/Timer/Message/TimerMessageHandler.php
@@ -5,11 +5,20 @@ declare(strict_types=1);
 namespace App\Timer\Message;
 
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class TimerMessageHandler implements MessageHandlerInterface
 {
+    public function __construct(private HttpClientInterface $client)
+    {
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
     public function __invoke(TimerMessage $message): void
     {
-        // TODO: implement
+        $this->client->request('POST', rtrim($message->getUrl(), '/') . '/' . $message->getId());
     }
 }
